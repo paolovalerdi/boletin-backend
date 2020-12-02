2e682951b5b66c3c341385fdf644ae7e3c684995
Paolo Valerdi <paolovalerdi@gmail.com>
2020-12-02 16:18:57 -0600

    Move express initialization to a class

--- src/app.ts
+++ src/app.ts
@@ -3,17 +3,22 @@ import newsRoutes from "../src/routes/news";
 import morgan from "morgan";
 
 const app = express();
-const PORT_KEY = "port"
 
-app.set(PORT_KEY, 3000);
-app.set("json spaces", 2);
+class Application {
+    private PORT_KEY = "port";
 
-app.use(morgan("dev"))
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+    init() {
+        app.set(this.PORT_KEY, 3000);
+        app.set("json spaces", 2);
+        app.use(morgan("dev"))
+        app.use(express.json());
+        app.use(express.urlencoded({ extended: true }));
+        app.use("/news", newsRoutes);
+        app.listen(app.get(this.PORT_KEY), () => {
+            console.log(`Server started at port ${app.get(this.PORT_KEY)}`);
+        });
+    }
+}
 
-app.use("/news", newsRoutes);
-
-app.listen(app.get(PORT_KEY), () => {
-    console.log(`Server started at port ${app.get(PORT_KEY)}`);
-});
+const server = new Application()
+server.init();
