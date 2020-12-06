b38e930346158d2488b839bf2ac9f03405b1eace
paolovalerdi <paolovalerdi@gmail.com>
2020-12-06 16:11:48 -0600

    Refactor Application class

--- src/app.ts
+++ src/app.ts
@@ -2,23 +2,22 @@ import express from "express";
 import newsRoutes from "../src/routes/news";
 import morgan from "morgan";
 
-const app = express();
-
 class Application {
+    
+    private expressApp = express();
     private PORT_KEY = "port";
 
-    init() {
-        app.set(this.PORT_KEY, 3000);
-        app.set("json spaces", 2);
-        app.use(morgan("dev"))
-        app.use(express.json());
-        app.use(express.urlencoded({ extended: true }));
-        app.use("/news", newsRoutes);
-        app.listen(app.get(this.PORT_KEY), () => {
-            console.log(`Server started at port ${app.get(this.PORT_KEY)}`);
+    constructor() {
+        this.expressApp.set(this.PORT_KEY, 3000);
+        this.expressApp.set("json spaces", 2);
+        this.expressApp.use(morgan("dev"))
+        this.expressApp.use(express.json());
+        this.expressApp.use(express.urlencoded({ extended: true }));
+        this.expressApp.use("/news", newsRoutes);
+        this.expressApp.listen(this.expressApp.get(this.PORT_KEY), () => {
+            console.log(`Server started at port ${this.expressApp.get(this.PORT_KEY)}`);
         });
     }
 }
 
-const server = new Application()
-server.init();
+new Application();
